78345135ad9dc10fce1a5b7fd079060448ca13f9
polina k <pmkochkina@gmail.com>
2022-04-07 15:05:14 +0500

    add antd and try buttons

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,65 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Button, Radio } from 'antd';
+import { RadioChangeEvent } from 'antd/lib';
+import { ButtonSize } from 'antd/lib/button';
+import { DownloadOutlined } from '@ant-design/icons';
+
+type Props = {}
+
+type State = {
+  size: ButtonSize
 }
 
+class App extends React.Component<Props, State> {
+  constructor (props: Props) {
+    super(props)
+
+    this.state = {
+      size: 'large',
+    };
+  }
+  
+
+  handleSizeChange = (e: RadioChangeEvent) => {
+    this.setState({ size: e.target.value });
+  };
+
+  render() {
+    const { size } = this.state;
+    return (
+      <>
+        <Radio.Group value={size} onChange={this.handleSizeChange}>
+          <Radio.Button value="large">Large</Radio.Button>
+          <Radio.Button value="default">Default</Radio.Button>
+          <Radio.Button value="small">Small</Radio.Button>
+        </Radio.Group>
+        <br />
+        <br />
+        <Button type="primary" size={size}>
+          Primary
+        </Button>
+        <Button size={size}>Default</Button>
+        <Button type="dashed" size={size}>
+          Dashed
+        </Button>
+        <br />
+        <Button type="link" size={size}>
+          Link
+        </Button>
+        <br />
+        <Button type="primary" icon={<DownloadOutlined />} size={size} />
+        <Button type="primary" shape="circle" icon={<DownloadOutlined />} size={size} />
+        <Button type="primary" shape="round" icon={<DownloadOutlined />} size={size} />
+        <Button type="primary" shape="round" icon={<DownloadOutlined />} size={size}>
+          Download
+        </Button>
+        <Button type="primary" icon={<DownloadOutlined />} size={size}>
+          Download
+        </Button>
+      </>
+    );
+  }
+}
 export default App;
